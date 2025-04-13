b9e1105a1f172ce8cdf7739979034dae28dc1290
teftelidev <chcleverf@gmail.com>
2025-04-13 12:56:09 +0300

    Code refactored

--- libs/core.cpp
+++ libs/core.cpp
@@ -2,7 +2,6 @@
 
 using namespace nlohmann;
 
-// Добавляем пароль пользователя в файл
 AddResponse Core::add_user_password(const AddRequest& data) {
     
     AddResponse r;
@@ -16,70 +15,44 @@ AddResponse Core::add_user_password(const AddRequest& data) {
     } else {
         r.code = 501;
         r.comment = "[Error] Write data";
-
         return r;
     }
 
     std::ofstream user_db_write("base.json");
-    
-    // НЕ ЗАБЫТЬ УБРАТЬ ДУБЛИКАТЫ
-
-    if (user_db_write.is_open()) {
-            bool is_ud_found = false;
-        
-            json user_data = {
-                {"platform",    data.platform},
-                {"login",       data.login},
-                {"password",    data.password}
-            };
-
-            if (current_data.contains(data.userid)) {
-
-                std::cout << "Data from ID: " << data.login << std::endl;
-
-                for (auto &item : current_data[data.userid]) {
-
-                    if ( user_data.at("login") == item.at("login") && user_data.at("platform") == item.at("platform")) {
-
-                        item.at("password") = user_data.at("password");
-
-                        is_ud_found = true;
-
-                    }
-                }
-
-                if (!is_ud_found) {
-                    current_data[data.userid].push_back(user_data);
-                }
-                
-            }
-
-            else {
-                current_data[data.userid] = { user_data };
-            }
-            
-            user_db_write << current_data.dump(4) << std::endl;
-            user_db_write.close();
-
-    } else {
-        r.code = 502;
-        r.comment = "[Error] Write data";
-        
-        return r;
-    }
 
     r.code = 200;
     r.comment = "Successful!!!";
 
+    if (!user_db_write.is_open()) {
+        r.code = 502;
+        r.comment = "[Error] Write data";
+        return r;
+    }
+
+    bool is_ud_found = false;
+        
+    json user_data = {
+        {"platform",    data.platform},
+        {"login",       data.login},
+        {"password",    data.password}
+    };
+
+    if (current_data.contains(data.userid)) {
+        for (auto &item : current_data[data.userid]) {
+            if ( user_data.at("login") == item.at("login") && user_data.at("platform") == item.at("platform")) {
+                item.at("password") = user_data.at("password");
+                is_ud_found = true;
+            }
+        }
+        if (!is_ud_found) {
+            current_data[data.userid].push_back(user_data);
+        }
+    } else { 
+        current_data[data.userid] = { user_data };
+    }
+
+    user_db_write << current_data.dump(4) << std::endl;
+    user_db_write.close();
+
     return r;
 }
-
-// GetResponse Core::get_user_password(const GetRequest& data) {
-// }
-
-/* GetUserPassword : 
-*/ 
-
-
-// * -> 0xASDASD
-// & -> <znacheniye>
